9d66ea345a428c9b45a76ccd897e4d8a35d8c575
Fernando Amado <fp.amado@icloud.com>
2021-02-17 09:49:56 +0000

    2nd

--- force-app/main/default/lwc/contact/contact.js
+++ force-app/main/default/lwc/contact/contact.js
@@ -1,19 +1,6 @@
-import { LightningElement, api, track} from 'lwc';
-import getContacts from '@salesforce/apex/ContactsList.getContacts';
+import { LightningElement, api} from 'lwc';
+
 
 export default class Contact extends LightningElement {
-    @track contactsList = '[]';
-
-    //Load contact list when page is first rendered
-    connectedCallback(){
-        getContacts(
-        )
-        .then(result => {
-            this.contactsList = result;
-            console.log(result);
-        })
-        .catch(error => {
-            this.error = error;
-        });
-    }
+    @api name;
 }
--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import getContacts from '@salesforce/apex/ContactsList.getContacts';
 
-export default class ContactList extends LightningElement {}
+export default class ContactList extends LightningElement {
+    @track contactList = '[]';
+
+    connectedCallback(){
+        getContacts(
+        )
+        .then(result => {
+            this.contactList = result;
+        })
+        .catch(error => {
+            this.error = error;
+        })
+    }
+
+
+}
